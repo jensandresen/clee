92fc2bda148ad8b05e56e135fcf4fd212f960f56
Jens Andresen <jensandresen@gmail.com>
2015-05-15 23:01:20 +0200

    added implementations for type factories

---
+++ src/Clee.Tests/DefaultTypeFactory.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Clee.Tests
+{
+    public class DefaultTypeFactory : ITypeFactory
+    {
+        public object Resolve(Type commandType)
+        {
+            return Activator.CreateInstance(commandType);
+        }
+
+        public void Release(object obj)
+        {
+            var disposable = obj as IDisposable;
+            
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Clee.Tests/ManualTypeFactory.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Clee.Tests
+{
+    public class ManualTypeFactory : ITypeFactory
+    {
+        private readonly Dictionary<Type, Func<object>> _creators = new Dictionary<Type, Func<object>>();
+
+        public void Add<T>(Func<T> creator)
+        {
+            _creators.Add(typeof(T), creator as Func<object>);
+        }
+
+        public object Resolve(Type commandType)
+        {
+            Func<object> creator;
+            
+            if (_creators.TryGetValue(commandType, out creator))
+            {
+                return creator();
+            }
+
+            throw new NotSupportedException(string.Format("Unable to resolve type {0}", commandType.FullName));
+        }
+
+        public void Release(object obj)
+        {
+            var disposable = obj as IDisposable;
+
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
