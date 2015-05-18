5103dce606537ad32bd1679e5ccb7897e43c2bcb
Jens Andresen <jensandresen@gmail.com>
2015-05-18 22:27:58 +0200

    removed potential features

--- src/Clee.Core/ValueAttribute.cs
+++ src/Clee.Core/ValueAttribute.cs
@@ -5,7 +5,7 @@ namespace Clee
     public class ValueAttribute : Attribute
     {
         public bool IsOptional { get; set; }
-        public string Format { get; set; }
-        public string DefaultValue { get; set; }
+//        public string Format { get; set; }
+//        public string DefaultValue { get; set; }
     }
 }
--- src/Clee.Tests/TestArgumentsMapper.cs
+++ src/Clee.Tests/TestArgumentsMapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Xunit;
 
 namespace Clee.Tests
