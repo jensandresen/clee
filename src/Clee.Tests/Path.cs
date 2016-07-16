80de4e608370226c7072897d3f5b4182674ebe39
Jens Andresen <jensandresen@gmail.com>
2016-07-16 15:10:25 +0200

    implemented path value object

---
+++ src/Clee.Tests/Path.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clee.Tests
+{
+    public class Path
+    {
+        private readonly string _rootValue;
+        private readonly LinkedList<string> _segments = new LinkedList<string>();
+
+        public Path(string rootValue)
+        {
+            if (string.IsNullOrWhiteSpace(rootValue))
+            {
+                throw new ArgumentException(string.Format("Invalid value \"{0}\" used to initialize a path instance.", rootValue), "rootValue");
+            }
+
+            if (rootValue.Contains(" "))
+            {
+                throw new ArgumentException("Path cannot be initialized with a value containing spaces.", "rootValue");
+            }
+
+            _rootValue = rootValue;
+        }
+
+        public override string ToString()
+        {
+            var result = "/" + _rootValue;
+            
+            if (_segments.Count == 0)
+            {
+                return result;
+            }
+
+            return result + "/" + string.Join("/", _segments);
+        }
+
+        public void AddSegment(string segmentValue)
+        {
+            _segments.AddLast(segmentValue);
+        }
+
+        #region equality members
+
+        protected bool Equals(Path other)
+        {
+            return string.Equals(this.ToString(), other.ToString());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((Path) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public static bool operator ==(Path left, Path right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Path left, Path right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion
+
+        public static Path Parse(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentException("Cannot parse null to a valid Path.", "text");
+            }
+
+            var segments = text
+                .Replace(' ', '/')
+                .Split(new[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (segments.Length == 0)
+            {
+                throw new ArgumentException(string.Format("Parsing input of \"{0}\" yields empty or invalid path.", text));
+            }
+
+            var result = new Path(segments[0]);
+
+            for (var i = 1; i < segments.Length; i++)
+            {
+                result.AddSegment(segments[i]);
+            }
+
+            return result;
+        }
+
+        public static bool TryParse(string text, out Path instance)
+        {
+            instance = null;
+
+            try
+            {
+                instance = Parse(text);
+                return true;
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Clee.Tests/TestPath.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Xunit;
+
+namespace Clee.Tests
+{
+    public class TestPath
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("          ")]
+        [InlineData("foo bar")]
+        public void ctor_throws_exception_if_input_is_invalid(string input)
+        {
+            Assert.Throws<ArgumentException>(() => new Path(input));
+        }
+
+        [Theory]
+        [InlineData("foo")]
+        [InlineData("bar")]
+        [InlineData("baz")]
+        [InlineData("qux")]
+        public void to_string_returns_expected_when_initialized_with_single_value(string value)
+        {
+            var sut = new Path(value);
+
+            Assert.Equal("/" + value, sut.ToString());
+        }
+
+        [Fact]
+        public void to_string_returns_expected_when_single_segment_are_added_to_the_path()
+        {
+            var sut = new Path("foo");
+            sut.AddSegment("bar");
+
+            Assert.Equal("/foo/bar", sut.ToString());
+        }
+
+        [Fact]
+        public void to_string_returns_expected_when_multiple_segments_are_added_to_the_path()
+        {
+            var sut = new Path("foo");
+            sut.AddSegment("bar");
+            sut.AddSegment("baz");
+            sut.AddSegment("qux");
+
+            Assert.Equal("/foo/bar/baz/qux", sut.ToString());
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_equal_instances()
+        {
+            var left = new Path("foo");
+            var right = new Path("foo");
+
+            Assert.Equal(left, right);
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_equal_instances_with_segments()
+        {
+            var left = new Path("foo");
+            left.AddSegment("bar");
+
+            var right = new Path("foo");
+            right.AddSegment("bar");
+
+            Assert.Equal(left, right);
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_equal_instances_using_operators()
+        {
+            var left = new Path("foo");
+            var right = new Path("foo");
+
+            Assert.True(left == right);
+            Assert.False(left != right);
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_equal_instances_with_segments_using_operators()
+        {
+            var left = new Path("foo");
+            left.AddSegment("bar");
+
+            var right = new Path("foo");
+            right.AddSegment("bar");
+
+            Assert.True(left == right);
+            Assert.False(left != right);
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_non_equal_instances()
+        {
+            var left = new Path("foo");
+            var right = new Path("bar");
+
+            Assert.NotEqual(left, right);
+        }
+
+        [Fact]
+        public void returns_expected_when_comparing_two_non_equal_instances_using_operators()
+        {
+            var left = new Path("foo");
+            var right = new Path("bar");
+
+            Assert.True(left != right);
+            Assert.False(left == right);
+        }
+
+        [Theory]
+        [InlineData("foo", "/foo")]
+        [InlineData("/foo", "/foo")]
+        public void returns_expected_when_parsing_valid_simple_input(string input, string expected)
+        {
+            var result = Path.Parse(input);
+            Assert.Equal(expected, result.ToString());
+        }
+
+        [Theory]
+        [InlineData("foo bar", "/foo/bar")]
+        [InlineData("/foo/bar", "/foo/bar")]
+        [InlineData("foo  bar", "/foo/bar")]
+        [InlineData("foo   bar", "/foo/bar")]
+        [InlineData("foo               bar", "/foo/bar")]
+        [InlineData("/foo//bar", "/foo/bar")]
+        [InlineData("/foo///bar", "/foo/bar")]
+        [InlineData("//foo/bar", "/foo/bar")]
+        [InlineData("///foo/bar", "/foo/bar")]
+        [InlineData("/foo/bar/", "/foo/bar")]
+        [InlineData(" / foo / bar ", "/foo/bar")]
+        public void returns_expected_when_parsing_valid_input_with_segments(string input, string expected)
+        {
+            var result = Path.Parse(input);
+            Assert.Equal(expected, result.ToString());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("/")]
+        [InlineData(" /")]
+        [InlineData(" / ")]
+        public void throws_exception_when_parsing_invalid_input(string invalidInput)
+        {
+            Assert.Throws<ArgumentException>(() => Path.Parse(invalidInput));
+        }
+
+        [Theory]
+        [InlineData("foo")]
+        [InlineData("/foo")]
+        [InlineData("foo bar")]
+        [InlineData("/foo/bar")]
+        public void try_parse_returns_expected_when_parsing_valid_input(string input)
+        {
+            Path notUsedInstance;
+            var result = Path.TryParse(input, out notUsedInstance);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("/")]
+        public void try_parse_returns_expected_when_parsing_invalid_input(string input)
+        {
+            Path notUsedInstance;
+            var result = Path.TryParse(input, out notUsedInstance);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("foo", "/foo")]
+        [InlineData("foo bar", "/foo/bar")]
+        public void try_parse_returns_expected_path_instance_when_parsing_valid_input(string input, string expected)
+        {
+            Path instance;
+            Path.TryParse(input, out instance);
+
+            Assert.NotNull(instance);
+            Assert.Equal(expected, instance.ToString());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("/")]
+        public void try_parse_returns_expected_path_instance_when_parsing_invalid_input(string input)
+        {
+            Path instance;
+            Path.TryParse(input, out instance);
+
+            Assert.Null(instance);
+        }
+    }
+}
