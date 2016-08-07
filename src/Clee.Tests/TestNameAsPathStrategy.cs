b392b5db748c051737396e6df896df44c941c72a
Jens Andresen <jensandresen@gmail.com>
2016-08-07 20:59:42 +0200

    implemented default command path strategy

---
+++ src/Clee.Tests/TestNameAsPathStrategy.cs
@@ -0,0 +1,25 @@
+﻿using Clee.Routing;
+using Clee.Tests.TestDoubles;
+using Xunit;
+
+namespace Clee.Tests
+{
+    public class TestNameAsPathStrategy
+    {
+        [Fact]
+        public void is_command_path_strategy()
+        {
+            var sut = new NameAsPathStrategy();
+            Assert.IsAssignableFrom<ICommandPathStrategy>(sut);
+        }
+
+        [Fact]
+        public void returns_expected_path_from_command()
+        {
+            var sut = new NameAsPathStrategy();
+            var result = sut.GeneratePathFor(new CommandMetaData(typeof (DummyCommand)));
+
+            Assert.Equal(new Path("dummy"), result);
+        }
+    }
+}
---
+++ src/Clee/Routing/NameAsPathStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Clee.Routing
+{
+    public class NameAsPathStrategy : ICommandPathStrategy
+    {
+        public Path GeneratePathFor(CommandMetaData metaData)
+        {
+            return new Path(metaData.CommandName);
+        }
+    }
+}
